19bad5c19a7bf4376d9df8a05092d45269cc340c
GeorgeSangillo <georgesangillo@ymail.com>
2020-01-26 17:26:39 -0500

    list of animals assigned to each shedule was added to feedingScheduleHome

--- src/com/examples/ezoo/servlets/FeedingScheduleServlet.java
+++ src/com/examples/ezoo/servlets/FeedingScheduleServlet.java
@@ -1,7 +1,10 @@
 package com.examples.ezoo.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.examples.ezoo.dao.AnimalDAO;
 import com.examples.ezoo.dao.DAOUtilities;
 import com.examples.ezoo.dao.FeedingScheduleDAO;
 import com.examples.ezoo.model.FeedingSchedule;
@@ -22,15 +26,26 @@ public class FeedingScheduleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		// Grab a list of Feeding Schedules from the Database
-		FeedingScheduleDAO dao = DAOUtilities.getFeedingScheduleDao();
-		List<FeedingSchedule> fs = dao.getAllFeedingSchedules();
+		FeedingScheduleDAO daoFS = DAOUtilities.getFeedingScheduleDao();
+		AnimalDAO daoAnimal = DAOUtilities.getAnimalDao();
+		List<FeedingSchedule> feedingSchedules = daoFS.getAllFeedingSchedules();
+		Map<Long, List<Long>> animalsForSchedule = new HashMap<Long, List<Long>>();
+		// feedingSchedules
+		// add stuff to map
+		for (FeedingSchedule schedule : feedingSchedules) {
+			Long id = schedule.getScheduleID();
+			List<Long> animalIDList = daoAnimal.getAnimalsFromFeedingSchedule(id);
+			animalsForSchedule.put(id, animalIDList);
+		}
 
 		// Populate the list into a variable that will be stored in the session
-		request.getSession().setAttribute("fs", fs);
+		request.getSession().setAttribute("feedingSchedules", feedingSchedules);
+		request.getSession().setAttribute("fsAnimalMap", animalsForSchedule);
 		
 		request.getRequestDispatcher("feedingScheduleHome.jsp").forward(request, response);
 	}
