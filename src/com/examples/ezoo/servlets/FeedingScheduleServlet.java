f2e1278f079aa6a93abf75c9393a1253d5841ed4
GeorgeSangillo <georgesangillo@ymail.com>
2020-01-26 15:29:41 -0500

    created servlet to have list in fsHome

---
+++ src/com/examples/ezoo/servlets/FeedingScheduleServlet.java
@@ -0,0 +1,37 @@
+package com.examples.ezoo.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.examples.ezoo.dao.DAOUtilities;
+import com.examples.ezoo.dao.FeedingScheduleDAO;
+import com.examples.ezoo.model.FeedingSchedule;
+
+
+/**
+ * Servlet implementation class FeedingScheduleServlet
+ */
+@WebServlet(description = "This servlet is the main interface into the Feeding Schedule Home", urlPatterns = { "/feedingScheduleHome" })
+public class FeedingScheduleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Grab a list of Feeding Schedules from the Database
+		FeedingScheduleDAO dao = DAOUtilities.getFeedingScheduleDao();
+		List<FeedingSchedule> fs = dao.getAllFeedingSchedules();
+
+		// Populate the list into a variable that will be stored in the session
+		request.getSession().setAttribute("fs", fs);
+		
+		request.getRequestDispatcher("feedingScheduleHome.jsp").forward(request, response);
+	}
+}
